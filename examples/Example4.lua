4c25600f311d3d85a18c027067afd5aa5f317458
Les Farrell <10669443+LesFarrell@users.noreply.github.com>
2022-09-20 11:54:01 +0100

    Add files via upload

---
+++ examples/Example4.lua
@@ -0,0 +1,10 @@
+archive = require("lzip")
+
+zip = archive.open("example_one.zip", 0, "r")
+
+zip:entry_open("File_One.txt");
+zip:entry_fread("extracted.txt");
+zip:entry_close();
+
+zip:close();
+
