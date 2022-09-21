0c484faaeef98237c4ead18fee6f2bbed7f9cdc6
Les Farrell <LesFarrell@gmail.com>
2022-09-21 09:39:54 +0100

    Added a bin folder to hold the compiled DLL.

--- examples/Example4.lua
+++ examples/Example4.lua
@@ -3,7 +3,7 @@ archive = require("lzip")
 zip = archive.open("example_one.zip", 0, "r")
 
 zip:entry_open("File_One.txt");
-zip:entry_fread("extracted.txt");
+zip:entry_fread("example4.txt");
 zip:entry_close();
 
 zip:close();
